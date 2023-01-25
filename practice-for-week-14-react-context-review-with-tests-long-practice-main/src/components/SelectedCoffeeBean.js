5c8a4be86b611a4770cfa687c0645954225d4b69
Daniel Lee <dlee13195@gmail.com>
2023-01-25 14:32:25 -0500

    phase 1

--- practice-for-week-14-react-context-review-with-tests-long-practice-main/src/components/SelectedCoffeeBean.js
+++ practice-for-week-14-react-context-review-with-tests-long-practice-main/src/components/SelectedCoffeeBean.js
@@ -1,5 +1,22 @@
+import { CoffeeContext, useCoffeeContext } from "../context/CoffeeContext"
+import { useEffect, useState, useContext } from 'react';
+import { useParams } from 'react-router-dom'
+
 const SelectedCoffeeBean = () => {
-  return null;
+  console.log("Rendering...")
+  
+  // const [data, setData] = useState([]);
+  // const { coffeeName } = useParams();
+
+  const { coffeeBean } = useCoffeeContext();
+
+  return (
+    <div className="selected-coffee">
+      <h2>
+        { coffeeBean.name }
+      </h2>
+    </div>
+  )
 }
 
 export default SelectedCoffeeBean;
